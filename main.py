34740d05e2be03e5894afbfcbc2b69b90cb1f6dd
bjellesma <william.jellesma@gmail.com>
2025-04-26 14:03:39 -0400

    tutorial 4: unit testing

--- main.py
+++ main.py
@@ -1,45 +1,9 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
 
-st.set_page_config(page_title="Crossfit Data")
-
-@st.cache_data
-def load_data() -> pd.DataFrame:
-    return pd.read_csv('temp/athletes.csv')
-
-@st.cache_data
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    args:
-        df (pd.Dataframe) data to be looking
-    """
-    filtered_df = df.dropna(subset=['weight', 'deadlift'])
-    filtered_df = filtered_df[(filtered_df['weight'] <= 500) & (filtered_df['deadlift'] <= 1000)
-                              & (filtered_df['weight'] >= 100) & (filtered_df['deadlift'] >= 10)]
-    return filtered_df.head(1000)
-
-def generate_scatter_plot(df: pd.DataFrame, x_axis:str="weight",y_axis:str="deadlift", trendline: str="ols") -> px.scatter:
-    fig = px.scatter(
-        df,
-        x=x_axis,
-        y=y_axis,
-        title=f'{x_axis} vs {y_axis}',
-        trendline=trendline,
-        hover_name='name',
-        hover_data=['region', 'affiliate', 'team', 'gender']
-    )
-
-    fig.update_layout(
-        xaxis_title=x_axis,
-        yaxis_title=y_axis,
-        height=700,
-        width=900
-    )
-
-    return fig
 
 def main():
+    import streamlit as st
+    from src.plot import load_data, clean_data, generate_scatter_plot
+    st.set_page_config(page_title="Crossfit Data")
     df = load_data()
     df = clean_data(df=df)
 
