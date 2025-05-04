56f5ecb84b7a2e5b18a0270ab52f0ecca6517818
bjellesma <william.jellesma@gmail.com>
2025-05-04 17:37:50 -0400

    episode 5 - improved data cleaning

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 def main():
     import streamlit as st
-    from src.plot import load_data, clean_data, generate_scatter_plot
+    from src.plot import load_data, clean_data, generate_scatter_plot, generate_histogram
     st.set_page_config(page_title="Crossfit Data")
     df = load_data()
-    df = clean_data(df=df)
+    
+    
 
     with st.sidebar:
         st.subheader('Options')
@@ -21,6 +22,12 @@ def main():
             options=[col for col in numeric_columns if col != x_axis],
             key='y_axis'
         )
+        x_threshold_lower = st.number_input(f'Select lower bound for {x_axis}',0,1000,0,1)
+        x_threshold_upper = st.number_input(f'Select upper bound for {x_axis}',0,1000,1000,1)
+        y_threshold_lower = st.number_input(f'Select lower bound for {y_axis}',0,1000,0,1)
+        y_threshold_upper = st.number_input(f'Select lower bound for {y_axis}',0,1000,1000,1)
+        x_threshold = (x_threshold_lower, x_threshold_upper)
+        y_threshold = (y_threshold_lower, y_threshold_upper)
         trendline_options={
             'Ordinary Least Squares': 'ols',
             'Expanding': 'expanding',
@@ -32,15 +39,25 @@ def main():
             options=trendline_options.keys(),
             key='trendline'
         )
+        sample_size = st.radio("Select a sample size", options=[1000, 10000, 100000])
         trendline = trendline_options[trendline_display]
+        df = clean_data(df=df, sample_size=sample_size, x_axis=x_axis, y_axis=y_axis, x_thresholds=x_threshold, y_thresholds=y_threshold)
 
-    st.plotly_chart(generate_scatter_plot(df=df, x_axis=x_axis, y_axis=y_axis, trendline=trendline))
-    if st.checkbox("Show Statistics"):
-        st.subheader("Averages")
-        st.markdown(f"**{x_axis}**: {df[x_axis].mean():.2f}")
-        st.markdown(f"**{y_axis}**: {df[y_axis].mean():.2f}")
-    with st.expander('Raw Data'):
-        st.dataframe(df)
+    
+    scatter_tab, stabs_tab = st.tabs(['Scatter', 'Stats'])
+    with scatter_tab:
+        st.plotly_chart(generate_scatter_plot(df=df, x_axis=x_axis, y_axis=y_axis, trendline=trendline))
+        if st.checkbox("Show Statistics"):
+            st.subheader("Averages")
+            st.markdown(f"**{x_axis}**: {df[x_axis].mean():.2f}")
+            st.markdown(f"**{y_axis}**: {df[y_axis].mean():.2f}")
+        with st.expander('Raw Data'):
+            st.dataframe(df)
+    with stabs_tab:
+        x_histogram=generate_histogram(df, x_axis)
+        y_histogram=generate_histogram(df, y_axis)
+        st.plotly_chart(x_histogram, use_container_width=True, theme=None)
+        st.plotly_chart(y_histogram, use_container_width=False, theme="streamlit")
 
 if __name__ == '__main__':
     main()
--- src/plot.py
+++ src/plot.py
@@ -7,15 +7,15 @@ def load_data() -> pd.DataFrame:
     return pd.read_csv('temp/athletes.csv')
 
 @st.cache_data
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+def clean_data(df: pd.DataFrame, sample_size: int, x_axis: str, y_axis: str, x_thresholds: tuple[int, int], y_thresholds: tuple[int, int]) -> pd.DataFrame:
     """
     args:
         df (pd.Dataframe) data to be looking
     """
-    filtered_df = df.dropna(subset=['weight', 'deadlift'])
-    filtered_df = filtered_df[(filtered_df['weight'] <= 500) & (filtered_df['deadlift'] <= 1000)
-                              & (filtered_df['weight'] >= 100) & (filtered_df['deadlift'] >= 10)]
-    return filtered_df.head(1000)
+    filtered_df = df.dropna(subset=[x_axis, y_axis])
+    filtered_df = filtered_df[(filtered_df[x_axis] <= x_thresholds[1]) & (filtered_df[y_axis] <= y_thresholds[1])
+                              & (filtered_df[x_axis] >= x_thresholds[0]) & (filtered_df[y_axis] >= y_thresholds[0])]
+    return filtered_df.head(sample_size)
 
 def generate_scatter_plot(df: pd.DataFrame, x_axis:str="weight",y_axis:str="deadlift", trendline: str="ols") -> px.scatter:
     fig = px.scatter(
@@ -33,6 +33,17 @@ def generate_scatter_plot(df: pd.DataFrame, x_axis:str="weight",y_axis:str="dead
         yaxis_title=y_axis,
         height=700,
         width=900
+    )
+
+    return fig
+
+def generate_histogram(df: pd.DataFrame, column: str):
+    fig = px.histogram(
+        df,
+        x=column,
+        nbins=50,
+        opacity=.7,
+        title=f'Distribution of {column}'
     )
 
     return fig
