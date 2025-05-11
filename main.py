4dab9b8892ecee49f3f8178935fcb484afc2e2cf
bjellesma <william.jellesma@gmail.com>
2025-05-10 20:21:33 -0400

    episode 6 - deviations

--- main.py
+++ main.py
@@ -24,8 +24,24 @@ def main():
         )
         x_threshold_lower = st.number_input(f'Select lower bound for {x_axis}',0,1000,0,1)
         x_threshold_upper = st.number_input(f'Select upper bound for {x_axis}',0,1000,1000,1)
+        x_std_slider = st.slider(
+            "Remove outliers beyond deviation:",
+            min_value=0.0,
+            max_value=5.0,
+            value=3.0,
+            step=0.1,
+            key="x_slider"
+        ) if st.checkbox("Remove Outliers", key="x_outliers") else 0
         y_threshold_lower = st.number_input(f'Select lower bound for {y_axis}',0,1000,0,1)
         y_threshold_upper = st.number_input(f'Select lower bound for {y_axis}',0,1000,1000,1)
+        y_std_slider = st.slider(
+            "Remove outliers beyond deviation:",
+            min_value=0.0,
+            max_value=5.0,
+            value=3.0,
+            step=0.1,
+            key="y_slider"
+        ) if st.checkbox("Remove Outliers", key="y_outliers") else 0
         x_threshold = (x_threshold_lower, x_threshold_upper)
         y_threshold = (y_threshold_lower, y_threshold_upper)
         trendline_options={
@@ -41,7 +57,9 @@ def main():
         )
         sample_size = st.radio("Select a sample size", options=[1000, 10000, 100000])
         trendline = trendline_options[trendline_display]
-        df = clean_data(df=df, sample_size=sample_size, x_axis=x_axis, y_axis=y_axis, x_thresholds=x_threshold, y_thresholds=y_threshold)
+        df, (x_mean, x_std), (y_mean, y_std), (x_lower, x_upper), (y_lower, y_upper) = clean_data(df=df, sample_size=sample_size, x_axis=x_axis, y_axis=y_axis, x_thresholds=x_threshold, y_thresholds=y_threshold, standard_deviations=(x_std_slider, y_std_slider))
+
+
 
     
     scatter_tab, stabs_tab = st.tabs(['Scatter', 'Stats'])
@@ -54,10 +72,24 @@ def main():
         with st.expander('Raw Data'):
             st.dataframe(df)
     with stabs_tab:
-        x_histogram=generate_histogram(df, x_axis)
-        y_histogram=generate_histogram(df, y_axis)
+        x_histogram=generate_histogram(df, x_axis, x_mean, x_std)
         st.plotly_chart(x_histogram, use_container_width=True, theme=None)
+        col = st.columns(2)
+        with col[0]:
+            st.markdown(f"**{x_axis} mean**: {x_mean:.2f}")
+        with col[1]:
+            st.markdown(f"**{x_axis} std**: {x_std:.2f}")
+            st.markdown(f"**{x_axis} lower**: {x_lower:.2f}")
+            st.markdown(f"**{x_axis} upper**: {x_upper:.2f}")
+        y_histogram=generate_histogram(df, y_axis, y_mean, y_std)
         st.plotly_chart(y_histogram, use_container_width=False, theme="streamlit")
+        col = st.columns(2)
+        with col[0]:
+            st.markdown(f"**{y_axis} mean**: {y_mean:.2f}")
+        with col[1]:
+            st.markdown(f"**{y_axis} std**: {y_std:.2f}")
+            st.markdown(f"**{y_axis} lower**: {y_lower:.2f}")
+            st.markdown(f"**{y_axis} upper**: {y_upper:.2f}")
 
 if __name__ == '__main__':
     main()
--- src/plot.py
+++ src/plot.py
@@ -2,12 +2,17 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+def calculate_stats(df, column):
+    mean = df[column].mean()
+    std = df[column].std()
+    return mean, std
+
 @st.cache_data
 def load_data() -> pd.DataFrame:
     return pd.read_csv('temp/athletes.csv')
 
 @st.cache_data
-def clean_data(df: pd.DataFrame, sample_size: int, x_axis: str, y_axis: str, x_thresholds: tuple[int, int], y_thresholds: tuple[int, int]) -> pd.DataFrame:
+def clean_data(df: pd.DataFrame, sample_size: int, x_axis: str, y_axis: str, x_thresholds: tuple[int, int], y_thresholds: tuple[int, int], standard_deviations: tuple[int, int]) -> pd.DataFrame:
     """
     args:
         df (pd.Dataframe) data to be looking
@@ -15,7 +20,27 @@ def clean_data(df: pd.DataFrame, sample_size: int, x_axis: str, y_axis: str, x_t
     filtered_df = df.dropna(subset=[x_axis, y_axis])
     filtered_df = filtered_df[(filtered_df[x_axis] <= x_thresholds[1]) & (filtered_df[y_axis] <= y_thresholds[1])
                               & (filtered_df[x_axis] >= x_thresholds[0]) & (filtered_df[y_axis] >= y_thresholds[0])]
-    return filtered_df.head(sample_size)
+    filtered_df = filtered_df.head(sample_size)
+
+    x_mean, x_std = calculate_stats(filtered_df, x_axis)
+    y_mean, y_std = calculate_stats(filtered_df, y_axis)
+    x_deviation_option, y_deviation_option = standard_deviations
+
+    if x_deviation_option:
+        x_lower = x_mean - x_std * x_deviation_option
+        x_upper = x_mean + x_std * x_deviation_option
+        filtered_df = filtered_df[(filtered_df[x_axis] >= x_lower) & (filtered_df[x_axis] <= x_upper)]
+    else:
+        x_lower, x_upper = x_thresholds[0], x_thresholds[1]
+
+    if y_deviation_option:
+        y_lower = y_mean - y_std * y_deviation_option
+        y_upper = y_mean + y_std * y_deviation_option
+        filtered_df = filtered_df[(filtered_df[y_axis] >= y_lower) & (filtered_df[y_axis] <= y_upper)]
+    else:
+        y_lower, y_upper = y_thresholds[0], y_thresholds[1]
+
+    return filtered_df, (x_mean, x_std), (y_mean, y_std), (x_lower, x_upper), (y_lower, y_upper)
 
 def generate_scatter_plot(df: pd.DataFrame, x_axis:str="weight",y_axis:str="deadlift", trendline: str="ols") -> px.scatter:
     fig = px.scatter(
@@ -37,7 +62,7 @@ def generate_scatter_plot(df: pd.DataFrame, x_axis:str="weight",y_axis:str="dead
 
     return fig
 
-def generate_histogram(df: pd.DataFrame, column: str):
+def generate_histogram(df: pd.DataFrame, column: str, mean, std, num_std = 5):
     fig = px.histogram(
         df,
         x=column,
@@ -45,5 +70,29 @@ def generate_histogram(df: pd.DataFrame, column: str):
         opacity=.7,
         title=f'Distribution of {column}'
     )
+
+    fig.add_vline(
+        x=mean,
+        line_dash="solid",
+        line_color="green",
+        annotation_text="Mean",
+        annotation_position="top right"
+    )
+
+    for i in range(1, num_std+1):
+        fig.add_vline(
+            x=mean+std*i,
+            line_dash="dash",
+            line_color="red",
+            annotation_text=f"+{i}σ",
+            annotation_position="top right"
+        )
+        fig.add_vline(
+            x=mean-std*i,
+            line_dash="dash",
+            line_color="red",
+            annotation_text=f"-{i}σ",
+            annotation_position="top right"
+        )
 
     return fig
